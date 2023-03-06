e429adbb6cb544d606db5152617189a3e69c855a
Benifeat <127098971+Benifeat@users.noreply.github.com>
2023-03-06 12:24:50 +0200

    Add files via upload

---
+++ preassembler.h
@@ -0,0 +1,21 @@
+
+
+#ifndef MAMAN14_PRE_ASM_H
+#define MAMAN14_PRE_ASM_H
+
+#include "memory_utilities.h"
+#include "error_case.h"
+#include "assembler.h"
+
+/* skip the spaces and tabs in the beginning */
+void skipTabSpace(char *p);
+/* remove all spaces from a given string */
+void remSpaces(char *p);
+/* process to open the macros */
+void openMcr(char *amFileName, char *asFileName, int *error);
+/* check if we get to macro name after we past the block of this macro */
+int chkMcrName(mcrHead *mcr, char *line, FILE *asFile, FILE *source);
+/* check if we get to macro block */
+int chkWord (char *line, char *word);
+
+#endif /*MAMAN14_PRE_ASM_H*/
